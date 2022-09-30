7b745aa740f0324f8627488795b717b1f8798143
sahilkhirsaria <sahilkhirsaria.pysquad@gmail.com>
2022-09-30 11:06:08 +0530

    initial commit

---
+++ chatgram/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from chatgram.users.models import User
+from chatgram.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
